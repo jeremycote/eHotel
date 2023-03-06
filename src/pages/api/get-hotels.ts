707f8db6f0c269f1b95d05c8c0ee35807c51e597
Arthur Fetiveau <art.fetiveau@gmail.com>
2023-03-05 21:04:28 -0500

    Fixed query

--- src/pages/api/get-hotels.ts
+++ src/pages/api/get-hotels.ts
@@ -3,6 +3,7 @@ import { Hotel } from '@/src/types/Hotel';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 import sql from '../../lib/db';
+import postgres from 'postgres';
 
 export default async function handler(
   req: NextApiRequest,
@@ -10,19 +11,19 @@ export default async function handler(
 ) {
   const { getImages, getCategories, getPhoneNumbers, getEmails } = req.query;
 
-  var query = sql<Hotel[]>`SELECT hotels.* `;
+  let query = 'SELECT hotels.*';
 
   if (getImages === 'true') {
     query +=
       ", COALESCE(ARRAY_AGG(hotel_images.url) FILTER (WHERE hotel_images.url IS NOT NULL), '{}') AS images";
   }
 
-  query += 'FROM hotels';
+  query += ' FROM hotels';
 
   if (getImages === 'true') {
     query +=
       ' LEFT JOIN hotel_images ON hotels.hotel_id = hotel_images.hotel_id';
   }
 
-  res.status(200).json(await query);
+  res.status(200).json(await sql.unsafe(query));
 }
