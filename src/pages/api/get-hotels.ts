14d9664e35a5032227426bf955ebd8bcdbbb73cc
Jeremy <jeremymcote@gmail.com>
2023-03-07 08:50:42 -0500

    Complete get-hotels endpoint

--- src/pages/api/get-hotels.ts
+++ src/pages/api/get-hotels.ts
@@ -2,7 +2,6 @@
 import { Hotel } from '@/src/types/Hotel';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
-import { debug } from 'console';
 import sql from '../../lib/db';
 
 export default async function handler(
@@ -16,7 +15,7 @@ export default async function handler(
 
   if (getImages === 'true') {
     columns +=
-      ", COALESCE(ARRAY_AGG(hotel_images.url) FILTER (WHERE hotel_images.url IS NOT NULL), '{}') AS images";
+      ", COALESCE(ARRAY_AGG(DISTINCT hotel_images.url) FILTER (WHERE hotel_images.url IS NOT NULL), '{}') AS images";
     from +=
       ' LEFT JOIN hotel_images ON hotels.hotel_id = hotel_images.hotel_id';
   }
@@ -28,8 +27,22 @@ export default async function handler(
       ' LEFT JOIN hotel_categories ON hotels.hotel_id = hotel_categories.hotel_id';
   }
 
+  if (getPhoneNumbers === 'true') {
+    columns +=
+      ", COALESCE(ARRAY_AGG(DISTINCT hotel_phone_numbers.phone_number) FILTER (WHERE hotel_phone_numbers.phone_number IS NOT NULL), '{}') AS phone_numbers";
+    from +=
+      ' LEFT JOIN hotel_phone_numbers ON hotels.hotel_id = hotel_phone_numbers.hotel_id';
+  }
+
+  if (getEmails === 'true') {
+    columns +=
+      ", COALESCE(ARRAY_AGG(DISTINCT hotel_emails.email) FILTER (WHERE hotel_emails.email IS NOT NULL), '{}') AS emails";
+    from +=
+      ' LEFT JOIN hotel_emails ON hotels.hotel_id = hotel_emails.hotel_id';
+  }
+
   let query = `SELECT ${columns} FROM ${from} GROUP BY hotels.hotel_id ORDER BY hotels.hotel_id`;
-  debug(query);
+  // debug(query);
 
   res.status(200).json(await sql.unsafe(query));
 }
