3090e779694566a0ffe62fcb662cf3a000467ca4
Jeremy Cote <jeremymcote@gmail.com>
2023-03-09 12:45:49 -0500

    Working employee auth

--- src/pages/api/employees/get-hotel-reservations.ts
+++ src/pages/api/employees/get-hotel-reservations.ts
@@ -1,6 +1,6 @@
 import sql from '@/src/lib/db';
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { Reservation } from '@/src/types/Reservation';
+import Reservation from '@/src/types/Reservation';
 import { getServerSession } from 'next-auth';
 import { authOptions } from '../auth/[...nextauth]';
 
@@ -10,24 +10,26 @@ export default async function handler(
 ) {
   const session = await getServerSession(req, res, authOptions);
 
-  if (session?.user?.is_employee === true) {
-    const employeeId = session!.user.id;
-
-    if (employeeId) {
-      const reservations: Reservation[] = await sql<Reservation[]>`
-              SELECT r.reservation_id, r.room_id, r.price, r.archived, r.number_guests, json_agg(c.*) as client, json_agg(ro.*) as room
+  if (session?.user?.email != null) {
+    const reservations: Reservation[] = await sql<Reservation[]>`
+              SELECT r.reservation_id, r.room_id, r.price, r.archived, r.number_guests, json_agg(u.*) as user, json_agg(ro.*) as room
               FROM reservations r
-                       LEFT JOIN clients c on c.client_id = r.client_id
+                       LEFT JOIN users u on u.user_id = r.user_id
                        LEFT JOIN rooms ro on ro.room_id = r.room_id
-              WHERE ro.hotel_id = (SELECT hotel_id FROM employees WHERE employee_id = ${employeeId} LIMIT 1)
+              WHERE ro.hotel_id = (SELECT hotel_id FROM employees WHERE employee_id IN (
+                SELECT employee_id FROM employees WHERE employee_id IN (
+                    SELECT user_id FROM users WHERE email = ${
+                      session!.user.email
+                    }
+                ) LIMIT 1
+              ) LIMIT 1)
               GROUP BY r.reservation_id
           `;
 
-      if (reservations != null) {
-        res.status(200).json(reservations);
-      } else {
-        res.status(500);
-      }
+    if (reservations != null) {
+      res.status(200).json(reservations);
+    } else {
+      res.status(401);
     }
   } else {
     res.status(401);
