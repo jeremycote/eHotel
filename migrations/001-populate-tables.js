4ce5ab7a519eef7e955ba007f9d00e3091879767
Arthur Fetiveau <art.fetiveau@gmail.com>
2023-03-23 11:41:12 -0400

    Fix: users

--- migrations/001-populate-tables.js
+++ migrations/001-populate-tables.js
@@ -137,44 +137,22 @@ exports.up = async function (sql) {
 
   await sql`
     INSERT INTO users (name, address, nas, email, phone_number, created_at, password) VALUES
-        ('Art Festival', '300 example rd', '11111111', 'art@example.com', '613 123-1234', '2020-12-20', crypt('hello', gen_salt('bf'))),
-        ('Voyageur Yogourt', '30 alder rd', '1141611', 'yogourt@example.com', '613 123-2234', '2019-12-20', crypt('hello', gen_salt('bf'))),
-        ('Voyageur Yogourt', '30 alder rd', '1141611', 'a@example.com', '613 123-2234', '2019-12-20', crypt('hello', gen_salt('bf'))),
-        ('Voyageur Yogourt', '30 alder rd', '1141611', 'b@example.com', '613 123-2234', '2019-12-20', crypt('hello', gen_salt('bf'))),
-        ('Voyageur Yogourt', '30 alder rd', '1141611', 'c@example.com', '613 123-2234', '2019-12-20', crypt('hello', gen_salt('bf'))),
-        ('Voyageur Yogourt', '30 alder rd', '1141611', 'd@example.com', '613 123-2234', '2019-12-20', crypt('hello', gen_salt('bf'))),
-        ('Voyageur Yogourt', '30 alder rd', '1141611', 'e@example.com', '613 123-2234', '2019-12-20', crypt('hello', gen_salt('bf'))),
-        ('Voyageur Yogourt', '30 alder rd', '1141611', 'f@example.com', '613 123-2234', '2019-12-20', crypt('hello', gen_salt('bf'))),
-        ('Voyageur Yogourt', '30 alder rd', '1141611', 'g@example.com', '613 123-2234', '2019-12-20', crypt('hello', gen_salt('bf'))),
-        ('Voyageur Yogourt', '30 alder rd', '1141611', 'h@example.com', '613 123-2234', '2019-12-20', crypt('hello', gen_salt('bf'))),
-        ('Voyageur Yogourt', '30 alder rd', '1141611', 'i@example.com', '613 123-2234', '2019-12-20', crypt('hello', gen_salt('bf'))),
-        ('Voyageur Yogourt', '30 alder rd', '1141611', 'j@example.com', '613 123-2234', '2019-12-20', crypt('hello', gen_salt('bf'))),
-        ('Voyageur Yogourt', '30 alder rd', '1141611', 'k@example.com', '613 123-2234', '2019-12-20', crypt('hello', gen_salt('bf'))),
-        ('Voyageur Yogourt', '30 alder rd', '1141611', 'l@example.com', '613 123-2234', '2019-12-20', crypt('hello', gen_salt('bf'))),
-        ('Voyageur Yogourt', '30 alder rd', '1141611', 'm@example.com', '613 123-2234', '2019-12-20', crypt('hello', gen_salt('bf'))),
-        ('Voyageur Yogourt', '30 alder rd', '1141611', 'n@example.com', '613 123-2234', '2019-12-20', crypt('hello', gen_salt('bf'))),
-        ('Voyageur Yogourt', '30 alder rd', '1141611', 'o@example.com', '613 123-2234', '2019-12-20', crypt('hello', gen_salt('bf'))),
-        ('Voyageur Yogourt', '30 alder rd', '1141611', 'p@example.com', '613 123-2234', '2019-12-20', crypt('hello', gen_salt('bf'))),
-        ('Voyageur Yogourt', '30 alder rd', '1141611', 'q@example.com', '613 123-2234', '2019-12-20', crypt('hello', gen_salt('bf'))),
-        ('Voyageur Yogourt', '30 alder rd', '1141611', 'r@example.com', '613 123-2234', '2019-12-20', crypt('hello', gen_salt('bf'))),
-        ('Voyageur Yogourt', '30 alder rd', '1141611', 's@example.com', '613 123-2234', '2019-12-20', crypt('hello', gen_salt('bf'))),
-        ('Voyageur Yogourt', '30 alder rd', '1141611', 't@example.com', '613 123-2234', '2019-12-20', crypt('hello', gen_salt('bf'))),
-        ('Voyageur Yogourt', '30 alder rd', '1141611', 'u@example.com', '613 123-2234', '2019-12-20', crypt('hello', gen_salt('bf'))),
-        ('Voyageur Yogourt', '30 alder rd', '1141611', 'v@example.com', '613 123-2234', '2019-12-20', crypt('hello', gen_salt('bf'))),
-        ('Voyageur Yogourt', '30 alder rd', '1141611', 'w@example.com', '613 123-2234', '2019-12-20', crypt('hello', gen_salt('bf'))),
-        ('Voyageur Yogourt', '30 alder rd', '1141611', 'x@example.com', '613 123-2234', '2019-12-20', crypt('hello', gen_salt('bf'))),
-        ('Voyageur Yogourt', '30 alder rd', '1141611', 'y@example.com', '613 123-2234', '2019-12-20', crypt('hello', gen_salt('bf'))),
-        ('Voyageur Yogourt', '30 alder rd', '1141611', 'z@example.com', '613 123-2234', '2019-12-20', crypt('hello', gen_salt('bf')));
-    `;
+        ('Employee', '300 example rd', '11111111', 'employee@hotel.com', '613 123-1234', '2020-12-20', crypt('employee', gen_salt('bf'))),
+        ('Employee 2', '300 example rd', '11111111', 'employee2@hotel.com', '613 123-1234', '2020-12-20', crypt('employee', gen_salt('bf'))),
+        ('Client', '30 alder rd', '1141611', 'client@hotel.com', '613 123-2234', '2019-12-20', crypt('client', gen_salt('bf'))),
+        ('Client 2', '30 alder rd', '1141611', 'client2@hotel.com', '613 123-2234', '2019-12-20', crypt('client', gen_salt('bf')))
+  `;
 
   await sql`
     INSERT INTO employees (employee_id, hotel_id) VALUES
-        (1, 1);
+        (1, 1),
+        (2, 2);
     `;
 
   await sql`
     INSERT INTO employee_roles (employee_id, role) VALUES
-        (1, 'Manager');
+        (1, 'Manager'),
+        (2, 'Manager');
     `;
 
   await sql`
